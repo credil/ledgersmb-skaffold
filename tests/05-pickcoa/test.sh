fef383021a747652286b8ac0d18d5033db586ef7
Michael Richardson <mcr@sandelman.ca>
2012-03-10 20:34:31 -0500

    test for picking the COA

---
+++ tests/05-pickcoa/test.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+where=$(pwd)
+while [ ! -f can-o-pg.settings ]; do
+    if [ -f etc ]; then echo "failed to find top"; exit 3; fi
+    cd ..
+done
+TOP=$(pwd)
+
+### 
+cd tests/05-pickcoa
+
+unset MAKEFLAGS
+unset MAKELEVEL
+owner=ledgersmb
+pass=$(cd ${TOP} && make dbpass)
+url=$(cd ${TOP}  && make url)
+
+echo "DROP DATABASE newco;" | (cd ${TOP} && make psql )
+
+database=newco
+
+mkdir -p OUTPUT
+
+# create the database (same as -04)
+curl -s --include \
+     --data-urlencode "s_user=${owner}" \
+     --data-urlencode "s_password=${pass}" \
+     --data-urlencode "database=${database}" \
+     --data-urlencode "action=create_db" \
+    --user ${owner}:${pass} "${url}/setup.pl" >/dev/null
+
+# create the database using CA Chart-of-Accounts
+curl -s --include \
+     --data-urlencode "s_user=${owner}" \
+     --data-urlencode "s_password=${pass}" \
+     --data-urlencode "database=${database}" \
+     --data-urlencode "coa_lc=ca" \
+     --data-urlencode "action=select_coa" \
+    --user ${owner}:${pass} "${url}/setup.pl" |
+    perl -f ${TOP}/http-sanity.pl | \
+    tee OUTPUT/result05.txt | \
+    diff -b -u - expected05.txt
+
+
